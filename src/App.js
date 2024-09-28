985e891c62b1cc0dad6f159b7e67a26c2d255314
JoshuaPam <Joshua_Pamintuan@rcc.edu.ph>
2024-09-28 11:25:06 +0800

    my-react-app3

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SumOfTwoNumbers from './SumOfTwoNumbers';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <SumOfTwoNumbers />
+    </div>
+  );
+}
+
+export default App;
+
